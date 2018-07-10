316c73fce1a3fcc1a56a4c1126c521007756089d
yelena guliyeva <gulbadam@gulbadam.com>
2018-07-09 17:29:58 -0700

    pg

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const image = require('./controllers/image');
 
 
 const db = knex({
-    // client: 'pg',
+    client: 'pg',
     // connection: {
     //     host: '127.0.0.1',
     //     user: 'gulbadam',
